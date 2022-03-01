957d42faa2f82feca2d3b00ad02bcbf5e61f16a6
bvasilop <bvasilop@gmail.com>
2022-03-01 12:52:53 -0800

    added dynamic styling with classes

--- Course-Todo-App/src/components/CourseGoals/CourseInput/CourseInput.js
+++ Course-Todo-App/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -23,18 +23,11 @@ const CourseInput = (props) => {
     props.onAddGoal(enteredValue);
   };
 
-  const labelStyle = { color: !isValid ? "red" : "black" };
-  const inputStyle = {
-    borderColor: !isValid ? "red" : "#ccc",
-    background: !isValid ? "salmon" : "transparent",
-  };
-
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={labelStyle}>Course Goal</label>
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+        <label>Course Goal</label>
         <input
-          style={inputStyle}
           type="text"
           onChange={goalInputChangeHandler}
         />
