68ac762789fe7abba6fb8bcfb42ed4470b96e9cd
bvasilop <bvasilop@gmail.com>
2022-03-01 13:49:13 -0800

    added FormControl styled comp. to CourseInput comp.

--- Course-Todo-App/src/components/UI/Button/Button.js
+++ Course-Todo-App/src/components/UI/Button/Button.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Button = styled.button`
   font: inherit;
@@ -9,19 +9,17 @@ const Button = styled.button`
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
 
-&:focus {
-  outline: none;
-}
+  &:focus {
+    outline: none;
+  }
 
-&:hover,
-&:active {
-  background: #ac0e77;
-  border-color: #ac0e77;
-  box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-}
+  &:hover,
+  &:active {
+    background: #ac0e77;
+    border-color: #ac0e77;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
 `;
-
-
 
 // const Button = ({type, onClick, children }) => {
 //   return (
