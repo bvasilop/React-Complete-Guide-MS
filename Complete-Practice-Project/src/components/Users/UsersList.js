d8b9df46337e89e67a726de12a154c65ff242671
bvasilop <bvasilop@gmail.com>
2022-03-17 14:34:26 -0700

    added modal with error handling

--- Complete-Practice-Project/src/components/Users/UsersList.js
+++ Complete-Practice-Project/src/components/Users/UsersList.js
@@ -2,11 +2,11 @@ import React from "react";
 import Card from "../UI/Card";
 import classes from "./UsersList.module.css";
 
-const UsersList = (props) => {
+const UsersList = ({ users }) => {
   return (
     <Card className={classes.users}>
       <ul>
-        {props.users.map((user) => (
+        {users.map((user) => (
           <li key={user.id}>
             {user.name} ({user.age} years old)
           </li>
