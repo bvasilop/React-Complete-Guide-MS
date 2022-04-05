8b4df5447ea70d794395e65568f4d7f5df314dde
bvasilop <bvasilop@gmail.com>
2022-04-05 12:35:04 -0700

    added useEffect with cleanup for password validation

--- UseEffect-Project/src/components/Login/Login.js
+++ UseEffect-Project/src/components/Login/Login.js
@@ -12,6 +12,13 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
+    console.log("Effect Running");
+    return () => {
+      console.log("EFFECT CLEANUP");
+    };
+  }, []);
+
+  useEffect(() => {
     const identifier = setTimeout(() => {
       console.log("Checking form validity!");
       setFormIsValid(
